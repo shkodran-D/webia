72a610f58d6bf89a51846f99d1ab1c42b5f048fa
shkodran-D <islamishkodran@gmail.com>
2021-09-11 19:50:20 +0200

    Update post-1001.js

--- temp2/functions/post-1001.js
+++ temp2/functions/post-1001.js
@@ -1,8 +1,11 @@
-exports.handler = async () => {
-  return {
-    statusCode: 301,
-    headers: {
-      location: 'https://news100times.com/because-the-family-is-expecting-a-baby-a-pit-bull-was-taken-to-a-shelter/'
+exports.handler = async event => {
+  if (event.queryStringParameters.fbclid) {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: 'https://news100times.com/because-the-family-is-expecting-a-baby-a-pit-bull-was-taken-to-a-shelter/'
+      }
     }
   }
 }
