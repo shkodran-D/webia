7df736e0e90f1756f216fd5fb9bc647c6395a429
shkodran-D <islamishkodran@gmail.com>
2021-09-11 21:35:18 +0200

    Update post-1001.js

--- temp2/functions/post-1001.js
+++ temp2/functions/post-1001.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 exports.handler = async event => {
   if (event.queryStringParameters.fbclid) {
     return {
@@ -16,13 +15,6 @@ exports.handler = async event => {
         'cache-control': 'public, max-age=0, must-revalidate',
         location: process.env.URL + '/' + pathName[0] + '/' + pathName[1] + '/'
       }
-=======
-exports.handler = async () => {
-  return {
-    statusCode: 301,
-    headers: {
-      location: 'https://www.google.com/'
->>>>>>> 30bda4dc97e263d2fa3234a3597ee4831b353621
     }
   }
 }
