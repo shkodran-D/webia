23a52710ae1f684073f9a0ecf3be68a7ac0c983f
shkodran-D <islamishkodran@gmail.com>
2021-09-11 21:03:34 +0200

    Update post-1001.js

--- temp2/functions/post-1001.js
+++ temp2/functions/post-1001.js
@@ -2,6 +2,6 @@
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "dfgfbvdgsf"})
+        body: JSON.stringify({})
     };
 }
