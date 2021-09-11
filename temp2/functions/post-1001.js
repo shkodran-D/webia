cfa2c08b501fdc8539a362e95daadb5038bd5415
shkodran-D <islamishkodran@gmail.com>
2021-09-11 21:23:47 +0200

    Update post-1001.js

--- temp2/functions/post-1001.js
+++ temp2/functions/post-1001.js
@@ -1,20 +1,9 @@
 
-exports.handler = async event => {
-  if (event.queryStringParameters.fbclid) {
-    return {
-      statusCode: 301,
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
-        location: decodeURIComponent(event.queryStringParameters.url)
-      }
-    }
-  } else {
-    return {
-      statusCode: 301,
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
-        location: process.env.URL + '/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
-      }
+exports.handler = async () => {
+  return {
+    statusCode: 301,
+    headers: {
+      location: 'https://www.google.com/'
     }
   }
 }
