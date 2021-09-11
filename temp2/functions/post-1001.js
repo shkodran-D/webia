3d4496eb79cc879e6828b41544b1e311d1994675
shkodran-D <islamishkodran@gmail.com>
2021-09-11 21:26:44 +0200

    Update post-1001.js

--- temp2/functions/post-1001.js
+++ temp2/functions/post-1001.js
@@ -1,9 +1,18 @@
-
-exports.handler = async () => {
-  return {
-    statusCode: 301,
-    headers: {
-      location: 'https://www.google.com/'
+exports.handler = async event => {
+  if (event.headers.referer.includes('facebook')) {
+    return {
+      statusCode: 301,
+      headers: {
+        location: 'https://www.google.com/'
+      }
+    }
+  } else {
+    let pathName = location.pathname.split('/')[2].split('-')
+    return {
+      statusCode: 301,
+      headers: {
+        location: process.env.URL + pathName[0] + '/' + pathName[1]
+      }
     }
   }
 }
