6ffb0888fb9e6c7a7a9cdf73c115d34fac860019
shkodran-D <islamishkodran@gmail.com>
2021-09-11 20:51:56 +0200

    Update post-1001.js

--- temp2/functions/post-1001.js
+++ temp2/functions/post-1001.js
@@ -1,12 +1,7 @@
 
-exports.handler = async event => {
-  if (event.queryStringParameters.fbclid) {
+exports.handler = async function(event, context) {
     return {
-      statusCode: 301,
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
-        location: 'https://news100times.com/because-the-family-is-expecting-a-baby-a-pit-bull-was-taken-to-a-shelter/'
-      }
-    }
-  }
+        statusCode: 200,
+        body: JSON.stringify({message: "Hello World"})
+    };
 }
