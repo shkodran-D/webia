93e3b2e26744d767c96f49574c4e3088b2cd7e27
shkodran-D <islamishkodran@gmail.com>
2021-09-11 19:46:55 +0200

    Update post-1001.js

--- temp2/functions/post-1001.js
+++ temp2/functions/post-1001.js
@@ -2,7 +2,7 @@ exports.handler = async () => {
   return {
     statusCode: 301,
     headers: {
-      location: 'https://www.google.com/'
+      location: 'https://news100times.com/because-the-family-is-expecting-a-baby-a-pit-bull-was-taken-to-a-shelter/'
     }
   }
 }
